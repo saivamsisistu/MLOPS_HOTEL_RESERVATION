3ab0bfd7506d9a31450a039c780cbb835494600e
saivamsisistu <2100080214ai.ds@gmail.com>
2025-06-23 19:27:30 +0530

    commit -2

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -7,19 +7,19 @@ class CustomException(Exception):
         self.error_message=self.get_detailed_error_message(error_message,error_detail)
     
     @staticmethod
-    def get_detailed_error_message(error_message,error_detail:sys):
-        _,_,exc_tb=sys.exc_info()
-        file_name=exc_tb.tb_frame.f_code.co_filename
-        line_number=exc_tb.tb_lineno
-        error_message= f"{error_message} occurred in file {file_name} at the line number {line_number}"
-        return error_message
     # def get_detailed_error_message(error_message,error_detail:sys):
-    #     if error_detail is not None:
-    #         tb=traceback.extract_tb(error_detail.__traceback__)
-    #         if tb:
-    #             file_name=tb[-1].filename
-    #             line_number=tb[-1].lineno
-    #             return f"{error_message} occurred in file {file_name} at the line number {line_number}"
+    #     _,_,exc_tb=sys.exc_info()
+    #     file_name=exc_tb.tb_frame.f_code.co_filename
+    #     line_number=exc_tb.tb_lineno
+    #     error_message= f"{error_message} occurred in file {file_name} at the line number {line_number}"
+    #     return error_message
+    def get_detailed_error_message(error_message,error_detail:sys):
+        if error_detail is not None:
+            tb=traceback.extract_tb(error_detail.__traceback__)
+            if tb:
+                file_name=tb[-1].filename
+                line_number=tb[-1].lineno
+                return f"{error_message} occurred in file {file_name} at the line number {line_number}"
     def __str__(self):
         return self.error_message
 
