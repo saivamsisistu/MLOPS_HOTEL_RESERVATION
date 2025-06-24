99b10a457c415f718a080f44970bdd6bd9662daf
saivamsisistu <2100080214ai.ds@gmail.com>
2025-06-24 22:03:30 +0530

    commit

--- application.py
+++ application.py
@@ -38,5 +38,5 @@ def index():
         return render_template('index.html',prediction=prediction[0])
     return render_template('index.html',prediction=None)
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=8080)
 
